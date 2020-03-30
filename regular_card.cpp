d74a96a3fc800b72c1ef6170132553678b69b6a5
f0402041 <45139408+f0402041@users.noreply.github.com>
2020-03-31 02:33:14 +0800

    Create regular_card.cpp

---
+++ regular_card.cpp
@@ -0,0 +1,25 @@
+#include "regular_card.h"
+
+//TODO: Implement the constructor here竊똞nd set default value 0 for credits.
+RegularCard::RegularCard(int id, string na) {
+}
+
+//TODO: Print "Dear regular member {name},
+//             remaining benefits are {credits} credits. ".
+void RegularCard::check_member_benefits() const {
+}
+
+//TODO: Implement get_name function here.
+string RegularCard::get_name() const {
+}
+
+//TODO: Implement get_credits function here.
+int RegularCard::get_credits() const {
+}
+
+//TODO: Implement set_credits function here.
+void RegularCard::set_credits(int cre) {
+}
+
+
+
